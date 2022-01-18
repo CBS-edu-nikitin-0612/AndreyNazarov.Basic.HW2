cf3c798a1ac891e5723de5d84e5894181800b8fa
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-01-19 00:19:31 +0300

    feat: Task4

---
+++ Task4/Invoice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task4
+{
+    class Invoice
+    {
+        public readonly int account;
+        public readonly string customer;
+        public readonly string provider;
+        private string article;
+        private int quantity;
+        private double price;
+
+        public Invoice(int account, string customer, string provider, string article, int quantity, int price)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+            this.article = article;
+            this.quantity = quantity;
+            this.price = price;
+        }
+
+        public double GetAmount(bool withTax = true)
+        {
+            checked
+            {
+                if (withTax)
+                {
+
+                    return price * quantity * 1.2;
+                }
+                return price * quantity;
+            }
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Customer: {customer}" +
+                $"\nSupplier: {provider}" +
+                $"\nItem: {article}" +
+                $"\nQuantity: {quantity}" +
+                $"\nAmount without Tax: {GetAmount(false)}"
+                );
+        }
+
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var invoice = new Invoice(256314, "Test Customer", "Test Supplier", "Test article", 10, 1055);
+            invoice.PrintInfo();
+        }
+    }
+}
