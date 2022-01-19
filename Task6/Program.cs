9b07ae2c2c8508ebfdbc15b58d7ac748a0acb019
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-01-19 19:55:59 +0300

    feat:Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User("testLogin", "TestName", "TestSeconName", 42);
+            user.PrintInfo();
+            user.PrintInfo();
+        }
+    }
+}
---
+++ Task6/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task6
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public int Age { get; set; }
+        private DateTime registrationDate;
+
+        public DateTime RegistrationDate
+        {
+            get { return registrationDate; }
+        }
+
+        public User(string login, string name, string secondName, int age)
+        {
+            Login = login;
+            Name = name;
+            SecondName = secondName;
+            Age = age;
+            registrationDate = DateTime.Now.Date;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Login: {Login}\n" +
+                $"Name: {Name}\n" +
+                $"Second name: {SecondName}\n" +
+                $"Age: {Age}\n" +
+                $"Registration date: {RegistrationDate}");
+        }
+    }
+}
